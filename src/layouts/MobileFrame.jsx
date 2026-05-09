2e399678ba192ce15ff58c368fc2c24a968cbe18
auleian <aule.ian@students.mak.ac.ug>
2026-05-09 09:46:21 +0300

    enhancements

--- src/layouts/MobileFrame.jsx
+++ src/layouts/MobileFrame.jsx
@@ -1,7 +1,7 @@
 export default function MobileFrame({ children }) {
   return (
-    <div className="min-h-screen w-full flex bg-white sm:bg-zinc-100 sm:items-center sm:justify-center sm:p-6">
-      <div className="relative flex flex-col w-full bg-white overflow-hidden sm:w-[374px] sm:h-[812px] sm:rounded-[40px] sm:shadow-2xl sm:ring-1 sm:ring-black/5">
+    <div className="min-h-[100dvh] w-full flex bg-white sm:bg-zinc-100 sm:items-center sm:justify-center sm:p-6">
+      <div className="relative flex flex-col w-full h-[812px] bg-white overflow-hidden sm:w-[374px] sm:rounded-[40px] sm:shadow-2xl sm:ring-1 sm:ring-black/5 mx-auto">
         {children}
       </div>
     </div>
